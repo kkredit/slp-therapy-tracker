3b69d588f4f6ae6db4f1b3472a6e5383945b5572
Kevin Kredit <k.kredit.us@ieee.org>
2019-04-11 15:39:35 -0400

    Change bootstrap style to Yeti

--- src/Includes.js
+++ src/Includes.js
@@ -13,8 +13,8 @@ class Includes extends React.Component {
       <React.Fragment>
         <link
           rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/yeti/bootstrap.min.css"
+          integrity="sha384-w6tc0TXjTUnYHwVwGgnYyV12wbRoJQo9iMlC2KdkdmVvntGgzT9jvqNEF/uKaF4m"
           crossOrigin="anonymous"
         />
       </React.Fragment>
