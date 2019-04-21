bb38a832f4f360c6d5cd0090c5435e9c1e356221
Kevin Kredit <k.kredit.us@ieee.org>
2019-04-21 12:13:16 -0400

    Clean up data entry

--- src/session/DataEntry.js
+++ src/session/DataEntry.js
@@ -17,101 +17,74 @@ const GoalButton = (props) => {
 
 const GoalCarouselItem = (props) => {
   return (
-      <Container>
-        <Row>
-          <Col xs>
-            <h4 className="goalNum"> Goal {props.goal.number + 1} </h4>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs/>
-          <Col xs={3}>
-            <GoalButton faClass="fa fa-3x fa-check" onClick={props.goal.addSuccess} />
-          </Col>
-          <Col xs={3}>
-            <GoalButton faClass="fa fa-3x fa-circle-o" onClick={props.goal.addCued} />
-          </Col>
-          <Col xs={3}>
-            <GoalButton faClass="fa fa-3x fa-times" onClick={props.goal.addFail} />
-          </Col>
-          <Col xs/>
-        </Row>
-      </Container>
+    <Container>
+      <Row>
+        <Col xs>
+          <h4 className="goalNum"> Goal {props.goal.number + 1} </h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs/>
+        <Col xs={3}>
+          <GoalButton faClass="fa fa-3x fa-check" onClick={props.goal.addSuccess} />
+        </Col>
+        <Col xs={3}>
+          <GoalButton faClass="fa fa-3x fa-circle-o" onClick={props.goal.addCued} />
+        </Col>
+        <Col xs={3}>
+          <GoalButton faClass="fa fa-3x fa-times" onClick={props.goal.addFail} />
+        </Col>
+        <Col xs/>
+      </Row>
+    </Container>
   );
 }
 
-class GoalsCarousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      goals: props.goals,
-    };
-  }
-
-  render() {
-    const goalCarouselItems = this.state.goals.map((goal) => {
-      return (
-        <Carousel.Item>
-          <GoalCarouselItem key={goal.number} goal={goal} />
-        </Carousel.Item>
-      );
-    });
-
+const GoalsCarousel = (props) => {
+  const goalCarouselItems = props.goals.map((goal) => {
     return (
-      <Carousel className="goalsCarousel"
-                interval={null}
-                indicators={false}
-                prevIcon={<i className="fa fa-3x fa-angle-left carouselDirections" />}
-                nextIcon={<i className="fa fa-3x fa-angle-right carouselDirections" />} >
-        {goalCarouselItems}
-      </Carousel>
+      <Carousel.Item key={goal.number}>
+        <GoalCarouselItem goal={goal} />
+      </Carousel.Item>
     );
-  }
+  });
+
+  return (
+    <Carousel className="goalsCarousel"
+              interval={null}
+              indicators={false}
+              prevIcon={<i className="fa fa-3x fa-angle-left carouselDirections" />}
+              nextIcon={<i className="fa fa-3x fa-angle-right carouselDirections" />} >
+      {goalCarouselItems}
+    </Carousel>
+  );
 }
 
-class StudentRow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      student: props.student,
-    };
-  }
-
-  render() {
-    return (
-      <Row>
-        <Col xs={1}>
-          <h4 className="studentName"> Student {this.state.student.number + 1} </h4>
-        </Col>
-        <Col xs={11}>
-          <GoalsCarousel goals={this.state.student.goals} />
-        </Col>
-      </Row>
-    );
-  }
+const StudentRow = (props) => {
+  return (
+    <Row>
+      <Col xs={1}>
+        <h4 className="studentName"> Student {props.student.number + 1} </h4>
+      </Col>
+      <Col xs={11}>
+        <GoalsCarousel goals={props.student.goals} />
+      </Col>
+    </Row>
+  );
 }
 
-class StudentRows extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: props.students,
-    };
-  }
-
-  render() {
-    const studentRowEntries = this.state.students.map((student) => {
-      return (
-        <StudentRow key={student.number} student={student} />
-      );
-    });
-
+const StudentRows = (props) => {
+  const studentRowEntries = props.students.map((student) => {
     return (
-      <React.Fragment>
-        {studentRowEntries}
-      </React.Fragment>
+      <StudentRow key={student.number} student={student} />
     );
-  }
+  });
+
+  return (
+    <React.Fragment>
+      {studentRowEntries}
+    </React.Fragment>
+  );
 }
 
 class DataEntry extends React.Component {
@@ -144,42 +117,40 @@ class DataEntry extends React.Component {
     }
     else {
       return (
-        <React.Fragment>
-          <Container id="dataContainer">
-            <Row>
-              <Col xs={1}/>
-              <Col xs={2}>
-                <h3 className="nameLocation"> {this.state.provider.fname} {this.state.provider.lname} </h3>
-              </Col>
-              <Col xs={2}>
-                <h3 className="nameLocation"> {this.state.location.name} </h3>
-              </Col>
-              <Col xs={4}/>
-              <Col xs={1}>
-                <Button variant="danger" className="endButton" onClick={this.sessionDiscard}>
-                  <i className="fa fa-trash" /> Discard
-                </Button>
-              </Col>
-              <Col xs={1}>
-                <Button variant="success" className="endButton" onClick={this.sessionSubmit}>
-                  <i className="fa fa-save" /> Submit
-                </Button>
-              </Col>
-              <Col xs={1} />
-            </Row>
-            <Row>
-              <Col xs={1}>
-                <h3 className="dataColHdr"> Students </h3>
-              </Col>
-              <Col xs={1} />
-              <Col xs={9}>
-                <h3 className="dataColHdr"> Goals </h3>
-              </Col>
-              <Col xs={1} />
-            </Row>
-            <StudentRows students={this.state.students} />
-          </Container>
-        </React.Fragment>
+        <Container id="dataContainer">
+          <Row>
+            <Col xs={1}/>
+            <Col xs={2}>
+              <h3 className="nameLocation"> {this.state.provider.fname} {this.state.provider.lname} </h3>
+            </Col>
+            <Col xs={2}>
+              <h3 className="nameLocation"> {this.state.location.name} </h3>
+            </Col>
+            <Col xs={4}/>
+            <Col xs={1}>
+              <Button variant="danger" className="endButton" onClick={this.sessionDiscard}>
+                <i className="fa fa-trash" /> Discard
+              </Button>
+            </Col>
+            <Col xs={1}>
+              <Button variant="success" className="endButton" onClick={this.sessionSubmit}>
+                <i className="fa fa-save" /> Submit
+              </Button>
+            </Col>
+            <Col xs={1} />
+          </Row>
+          <Row>
+            <Col xs={1}>
+              <h3 className="dataColHdr"> Students </h3>
+            </Col>
+            <Col xs={1} />
+            <Col xs={9}>
+              <h3 className="dataColHdr"> Goals </h3>
+            </Col>
+            <Col xs={1} />
+          </Row>
+          <StudentRows students={this.state.students} />
+        </Container>
       );
     }
   }
